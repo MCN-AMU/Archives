12f0d782f01c40918cece4b235685b026bf08559
Stéphane Cousot <postmaster@ubaa.net>
2020-10-30 10:03:11 +0100

    Clavier led rgb project

---
+++ Clavier led rgb/Clavier led rgb/ledrgb/ledrgb.ino
@@ -0,0 +1,77 @@
+const byte COLOR_BLACK = 0b000;
+const byte COLOR_RED = 0b100;
+const byte COLOR_GREEN = 0b010;
+const byte COLOR_BLUE = 0b001;
+const byte COLOR_MAGENTA = 0b101;
+const byte COLOR_CYAN = 0b011;
+const byte COLOR_YELLOW = 0b110;
+const byte COLOR_WHITE = 0b111;
+
+/* Broches */
+const byte PIN_LED_R = 10;
+const byte PIN_LED_G = 9;
+const byte PIN_LED_B = 8;
+
+int capteur = A0;
+
+
+// Fonction setup(), appelée au démarrage de la carte Arduino
+void setup() {
+
+  Serial.begin(9600);
+
+  // Initialise les broches
+  pinMode(PIN_LED_R, OUTPUT);
+  pinMode(PIN_LED_G, OUTPUT);
+  pinMode(PIN_LED_B, OUTPUT);
+  displayColor(COLOR_BLACK);
+}
+
+// Fonction loop(), appelée continuellement en boucle tant que la carte Arduino est alimentée
+void loop() {
+
+  int lecture = analogRead(capteur);
+
+  Serial.print("lecture ");
+  Serial.println(lecture);
+  delay(250);
+  /* Code de démonstration */
+  displayColor(COLOR_RED);
+  delay(1000);
+
+  displayColor(COLOR_GREEN);
+  delay(1000);
+
+  displayColor(COLOR_BLUE);
+  delay(1000);
+
+  displayColor(COLOR_MAGENTA);
+  delay(1000);
+
+  displayColor(COLOR_CYAN);
+  delay(1000);
+
+  displayColor(COLOR_YELLOW);
+  delay(1000);
+
+  displayColor(COLOR_WHITE);
+  delay(1000);
+
+  displayColor(COLOR_BLACK);
+  delay(1000);
+}
+
+/** Affiche une couleur */
+void displayColor(byte color) {
+
+  // Assigne l'état des broches
+  // Version cathode commune
+  //digitalWrite(PIN_LED_R, bitRead(color, 2));
+  //digitalWrite(PIN_LED_G, bitRead(color, 1));
+  //digitalWrite(PIN_LED_B, bitRead(color, 0));
+  // Version anode commune
+  digitalWrite(PIN_LED_R, !bitRead(color, 2));
+  digitalWrite(PIN_LED_G, !bitRead(color, 1));
+  digitalWrite(PIN_LED_B, !bitRead(color, 0));
+}
+
---
+++ Clavier led rgb/README.md
@@ -0,0 +1,10 @@
+Projet 1 : Clavier RGB
+----------------------
+![Clavier RGB](clavier-led-rgb.jpg "Clavier RGB")  
+
+C'est un clavier créé avec des capteurs de pressions qui jouent des notes format MP3 et qui allument une LED RVB avec la possibilité de mélanger les couleurs entre elles.
+
+Projet 2 : Webcam détection rouge
+---------------------------------
+![Webcam détection rouge](webcam-detection-rouge.jpg "Webcam détection rouge")  
+Le projet est créé avec un écran et une webcam qui, à l'aide d'un repère numérique, capte la couleur rouge quand elle apparait à l'ecran.
